e1bd24f11a48d37bbe12c9ddeb816679466fbd0d
Vladas Zakrevskis <146100@gmail.com>
2017-08-01 15:51:50 +0300

    Added inset label

---
+++ Sources/InsetLabel.swift
@@ -0,0 +1,51 @@
+//
+//  InsetLabel.swift
+//  iOSTools
+//
+//  Created by Vladas Zakrevskis on 8/1/17.
+//  Copyright © 2017 VladasZ. All rights reserved.
+//
+
+import UIKit
+
+//https://stackoverflow.com/questions/3476646/uilabel-text-margin
+
+@IBDesignable class InsetLabel: UILabel {
+    
+    @IBInspectable var topInset: CGFloat = 0.0
+    @IBInspectable var leftInset: CGFloat = 0.0
+    @IBInspectable var bottomInset: CGFloat = 0.0
+    @IBInspectable var rightInset: CGFloat = 0.0
+    
+    var insets: UIEdgeInsets {
+        get {
+            return UIEdgeInsetsMake(topInset, leftInset, bottomInset, rightInset)
+        }
+        set {
+            topInset = newValue.top
+            leftInset = newValue.left
+            bottomInset = newValue.bottom
+            rightInset = newValue.right
+        }
+    }
+    
+    override func drawText(in rect: CGRect) {
+        super.drawText(in: UIEdgeInsetsInsetRect(rect, insets))
+    }
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        var adjSize = super.sizeThatFits(size)
+        adjSize.width += leftInset + rightInset
+        adjSize.height += topInset + bottomInset
+        
+        return adjSize
+    }
+    
+    override var intrinsicContentSize: CGSize {
+        var contentSize = super.intrinsicContentSize
+        contentSize.width += leftInset + rightInset
+        contentSize.height += topInset + bottomInset
+        
+        return contentSize
+    }
+}
