4ba58040e8d4b8470e11c353e145347ba2581077
Vladas Zakrevskis <146100@gmail.com>
2017-08-03 12:56:06 +0300

    defaults class

---
+++ Sources/Defaults.swift
@@ -0,0 +1,33 @@
+//
+//  Defaults.swift
+//  iOSTools
+//
+//  Created by Vladas Zakrevskis on 8/3/17.
+//  Copyright © 2017 VladasZ. All rights reserved.
+//
+
+import Foundation
+import SwiftyTools
+
+fileprivate let defaults = UserDefaults.standard
+
+
+public class Defaults {
+    
+    public static func setValue(_ value: Any?, forKey key: String) {
+    
+        defaults.set(value, forKey: key)
+    }
+    
+    public static func valueForKey<T>(_ key: String) -> T? {
+        
+        if let value = defaults.value(forKey: key) {
+            
+            if let tValue = value as? T { return tValue }
+            Log.error("Wrong defaults type for key: " + key)
+            return nil
+        }
+        
+        return nil
+    }
+}
