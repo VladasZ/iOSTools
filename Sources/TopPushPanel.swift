0665dfb9859e9012dd4599ca28faac902aa62b27
Vladas Zakrevskis <146100@gmail.com>
2017-08-14 12:14:40 +0300

    TopPushPanel fix

--- Sources/TopPushPanel.swift
+++ Sources/TopPushPanel.swift
@@ -9,25 +9,25 @@
 import UIKit
 import SwiftyTools
 
-class TopPushPanel {
+public class TopPushPanel {
     
     //MARK: - Properties
     
     private static var isVisible: Bool = false
-    static var view: UIView!
+    public static var view: UIView!
     
-    class var height: CGFloat { Log.error(); return CGFloat() }
+    public class var height: CGFloat { Log.error(); return CGFloat() }
     
-    static var visibilityDuration: Int! = 2
-    static var animationDuration: TimeInterval = 0.211
+    public static var visibilityDuration: Int! = 2
+    public static var animationDuration: TimeInterval = 0.211
     
-    class func setup() {
+    public class func setup() {
         
         view.frame = CGRect(x: 0, y: -height, width: keyWindow.width, height: height)
         keyWindow.addSubview(view)
     }
     
-    static func show() {
+    public static func show() {
         
         if view == nil { setup() }
         if isVisible { return }; isVisible = true
@@ -42,7 +42,7 @@ class TopPushPanel {
         }
     }
     
-    static func dismiss() {
+    public static func dismiss() {
         
         UIView.animate(withDuration: animationDuration, animations: { view.y = -height  }, completion: { _ in
             
