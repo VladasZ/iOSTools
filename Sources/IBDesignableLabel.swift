0ec6d1268f47ab498ca357744e6b7f0835fa29b6
Vladas Zakrevskis <146100@gmail.com>
2017-08-18 14:24:07 +0300

    optional cimport UIKit

---
+++ Sources/IBDesignableLabel.swift
@@ -0,0 +1,45 @@
+//
+//  IBDesignableLabel.swift
+//  iOSTools
+//
+//  Created by Vladas Zakrevskis on 8/18/17.
+//  Copyright © 2017 VladasZ. All rights reserved.
+//
+
+import UIKit
+import SwiftyTools
+
+@IBDesignable
+open class IBDesignableLabel : UILabel {
+    
+    private var identifier: String { return String(describing: type(of: self)) }
+    
+    //MARK: - Initialization
+    
+    public override init(frame: CGRect) {
+        
+        super.init(frame: frame)
+        addSubview(loadFromXib())
+    }
+    
+    public required init?(coder aDecoder: NSCoder) {
+        
+        super.init(coder: aDecoder)
+        addSubview(loadFromXib())
+    }
+    
+    private func loadFromXib() -> UIView {
+        
+        let bundle = Bundle(for: type(of: self))
+        let nib = UINib(nibName: identifier, bundle: bundle)
+        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        view.frame = self.bounds
+        view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        setup()
+        return view
+    }
+    
+    open func setup() {
+        
+    }
+}
