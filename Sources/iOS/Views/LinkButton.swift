e4ba6d26c8ccf55dbac0260131f8b1d51b56910b
Vladas Zakrevskis <146100@gmail.com>
2018-01-03 13:27:04 +0300

    LinkButton view

---
+++ Sources/iOS/Views/LinkButton.swift
@@ -0,0 +1,34 @@
+//
+//  LinkButton.swift
+//  iOSTools
+//
+//  Created by Vladas Zakrevskis on 1/3/18.
+//  Copyright © 2018 Vladas Zakrevskis. All rights reserved.
+//
+
+import UIKit
+import SwiftyTools
+
+public class LinkButton : UIButton {
+    
+    @IBInspectable public var link: String?
+    
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        setup()
+    }
+    
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+    
+    private func setup() {
+        addTarget(self, action: #selector(self.didTap), for: .touchUpInside)
+    }
+    
+    @objc private func didTap() {
+        guard let url = link?.url else { Log.error(); return }
+        System.openURL(url)
+    }
+}
