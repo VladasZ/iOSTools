9b71b936ce50706eb5d1e00ede9f39aca6428ca3
Vladas Zakrevskis <146100@gmail.com>
2018-01-15 15:45:20 +0300

    HKWorkoutSession convitience initializatior

---
+++ Sources/watchOS/HKWorkoutSessionTools.swift
@@ -0,0 +1,18 @@
+//
+//  HKWorkoutSessionTools.swift
+//  iOSTools
+//
+//  Created by Vladas Zakrevskis on 1/15/18.
+//  Copyright © 2018 Vladas Zakrevskis. All rights reserved.
+//
+
+import HealthKit
+
+public extension HKWorkoutSession {
+    
+    class func create(_ conf: (HKWorkoutConfiguration) -> ())  -> HKWorkoutSession? {
+        let workoutConfiguration = HKWorkoutConfiguration()
+        conf(workoutConfiguration)
+        return try? HKWorkoutSession(configuration: workoutConfiguration)
+    }
+}
