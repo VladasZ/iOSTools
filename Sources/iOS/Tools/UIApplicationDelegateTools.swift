23b0f2e10bfffe483c8bc67d49638e66e76f6805
Vladas Zakrevskis <146100@gmail.com>
2018-02-13 15:35:40 +0300

    AppDelegate changeRootController animated method

---
+++ Sources/iOS/Tools/UIApplicationDelegateTools.swift
@@ -0,0 +1,31 @@
+//
+//  UIApplicationDelegateTools.swift
+//  iOSTools
+//
+//  Created by Vladas Zakrevskis on 2/13/18.
+//  Copyright © 2018 Vladas Zakrevskis. All rights reserved.
+//
+
+import UIKit
+import SwiftyTools
+
+public extension UIApplicationDelegate {
+    
+    static func changeRootControllerAnimated(_ controller: UIViewController) {
+        
+        guard let _window = UIApplication.shared.delegate?.window else { Log.error(); return }
+        guard let window = _window else { Log.error(); return }
+        guard let snapshot = window.snapshotView(afterScreenUpdates: true) else { Log.error(); return }
+    
+        controller.view.addSubview(snapshot);
+        window.rootViewController = controller
+        
+        UIView.animate(withDuration: 0.3, animations: {() in
+            snapshot.layer.opacity = 0;
+            snapshot.layer.transform = CATransform3DMakeScale(1.5, 1.5, 1.5);
+        }, completion: {
+            (value: Bool) in
+            snapshot.removeFromSuperview();
+        });
+    }
+}
