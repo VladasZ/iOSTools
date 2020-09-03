206864a6dae83ed9556c0d06002b8cefeeac392c
Vladas Zakrevskis <146100@gmail.com>
2020-09-03 20:44:00 +0300

    uiimage resize

--- Sources/iOS/Tools/UIImageTools.swift
+++ Sources/iOS/Tools/UIImageTools.swift
@@ -35,6 +35,32 @@ public extension UIImage {
         UIGraphicsEndImageContext()
         return normalizedImage
     }
+    
+    func resize(targetSize: CGSize) -> UIImage {
+
+        let widthRatio  = targetSize.width  / size.width
+        let heightRatio = targetSize.height / size.height
+
+        // Figure out what our orientation is, and use that to form the rectangle
+        var newSize: CGSize
+        if(widthRatio > heightRatio) {
+            newSize = CGSize(size.width * heightRatio, size.height * heightRatio)
+        } else {
+            newSize = CGSize(size.width * widthRatio,  size.height * widthRatio)
+        }
+
+        // This is the rect that we've calculated out and this is what is actually used below
+        let rect = CGRect(0, 0, newSize.width, newSize.height)
+
+        // Actually do the resizing to the rect using the ImageContext stuff
+        UIGraphicsBeginImageContextWithOptions(newSize, false, 1.0)
+        draw(in: rect)
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()!
+        UIGraphicsEndImageContext()
+
+        return newImage
+    }
+    
 }
 
 #endif
